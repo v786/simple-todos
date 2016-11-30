ea66390c83ba15b06c71e9e1f1c091148cb1a32d
v786 <jahangirali786@gmail.com>
2016-11-30 19:46:36 +0530

    Log In Added.

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -18,18 +18,20 @@ export default class Task extends Component {
 
 		return (
 			<li className = {taskClassName}>
-			<button className = "delete" onClick = {this.deleteThisTask.bind(this)}>
-				&times;
-			</button>
+				<button className = "delete" onClick = {this.deleteThisTask.bind(this)}>
+					&times;
+				</button>
 
-			<input
-				type = "checkbox"
-				readOnly
-				checked = {this.props.task.checked}
-				onClick = {this.toggleChecked.bind(this)}
-			/>
+				<input
+					type = "checkbox"
+					readOnly
+					checked = {this.props.task.checked}
+					onClick = {this.toggleChecked.bind(this)}
+				/>
 
-			<span className = "text"> {this.props.task.text}</span>
+				<span className = "text">
+					<strong>{this.props.task.username}</strong>: {this.props.task.text}
+				</span>
 			</li>	
 		);
 	}
